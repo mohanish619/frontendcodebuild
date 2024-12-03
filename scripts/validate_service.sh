d7ad8baed926e5a82e86bb30d45f5e99c4c3a548
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2024-12-03 16:21:11 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo "Validating the application..."
-curl -f http://localhost || exit 1
-echo "Application validation successful."
+
+# Retry curl to check if the app is up, up to 5 times with 5-second intervals
+for i in {1..5}; do
+  curl -f http://localhost && break || sleep 5
+done
+
+if [ $? -eq 0 ]; then
+  echo "Application validation successful."
+else
+  echo "Application validation failed."
+  exit 1
+fi
