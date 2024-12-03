fc3ece60a93a022fbfae30922599d42a883b44c2
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2024-12-03 16:44:11 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "Starting the application..."
-docker stop my-frontend-app || true
-docker rm my-frontend-app || true
-docker pull 156041427059.dkr.ecr.us-east-2.amazonaws.com/my-frontend-app:latest
-docker run -d -p 80:3000 --name my-frontend-app 156041427059.dkr.ecr.us-east-2.amazonaws.com/my-frontend-app:latest
-echo "Application started successfully."
+echo "Starting the application in Docker..."
+docker run -d -p 80:80 --name my-frontend-app 156041427059.dkr.ecr.us-east-2.amazonaws.com/my-frontend-app:latest
